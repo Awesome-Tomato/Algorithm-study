471357749dd5d40ffe851fff72b22ea54bef26e4
chanwoo <chanwookim@me.com>
2022-03-13 04:04:11 +0900

    [Leetcode]125. Valid Palindrome
    문자열에서 특수문자들 지우는 방법에 따라
    실행속도가 많이 차이났음.(반복문->정규식)
    
    While loop에서 for loop로 변경

--- chapter01-string/01-valid-palindrome/chanwoo.java
+++ chapter01-string/01-valid-palindrome/chanwoo.java
@@ -1,2 +1,33 @@
-# test file
-# 나으 파일
+public class Leetcode125 {
+    public static void main(String[] args) {
+        var sol = new Solution();
+
+        System.out.println(sol.isPalindrome("A man, a plan, a canal: Panama"));
+
+    }
+
+    static class Solution {
+        public boolean isPalindrome(String s) {
+//             611ms
+//            var preProcessedString = s.toLowerCase().replace(" ", "");
+//
+//            for(var c : preProcessedString.toCharArray()){
+//                if(!Character.isLetterOrDigit(c)) {
+//                    preProcessedString = preProcessedString.replace(Character.toString(c), "");
+//                }
+//            }
+
+//            14ms
+            var preProcessedString = s.toLowerCase()
+                    .replaceAll("[^a-z0-9]", "");
+            var preProcessedCharArray = preProcessedString.toCharArray();
+            for(var index=0; index<preProcessedCharArray.length/2; index++) {
+                if (preProcessedCharArray[index] != preProcessedCharArray[preProcessedCharArray.length - index - 1]) {
+                    return false;
+                }
+                index++;
+            }
+            return true;
+        }
+    }
+}
