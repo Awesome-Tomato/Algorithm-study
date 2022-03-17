ee69fbdb9d073cd57677199795298ccad56618db
becover <48202230+becover@users.noreply.github.com>
2022-03-17 22:57:04 +0900

    fix edge case
    reverse메서드가 기존 레퍼런스를 수정하는것을 방지하기 위해 얕은복사로 스프레드로 떠서 리버스 시키고
    대소문자 엣지케이스를 커버하기위해 toLowerCase를 추가

--- chapter01-string/01-valid-palindrome/becover.js
+++ chapter01-string/01-valid-palindrome/becover.js
@@ -4,8 +4,8 @@
  */
 const isPalindrome = function(s) {
     const alphanumeric = /[A-Za-z0-9]/;
-    const filterAlphanumeric = [...s].filter(word=> alphanumeric.test(word));
-    const reverseFilteralphanumeric = filterAlphanumeric.reverse();
+    const filterAlphanumeric = [...s].filter(word=> alphanumeric.test(word)).map(w=>w.toLowerCase());
+    const reverseFilteralphanumeric = [...filterAlphanumeric].reverse();
     if(filterAlphanumeric.join("") === reverseFilteralphanumeric.join("")) {
         return true;
     } else {
