c2418b02c8349fcedfc27195e5c4a7a64e100045
SuKyoung Shin <sukyoung.dev@gmail.com>
2022-04-14 00:10:32 +0900

    feat : 6번 문제풀이 추가 (예외처리 완료)

--- chapter01-string/06-longest-palindromic-substring/sukyoung.js
+++ chapter01-string/06-longest-palindromic-substring/sukyoung.js
@@ -6,7 +6,7 @@
 const input1 = "babad"; 
 const input2 = "cbbd"; 
 const input3 = 'oiasjdfiojaweoijfoiwjefoaofijwfeijoaef';
-const input4 = 'abcbaaaabbbddddabcdcbaee';
+const input4 = 'abadd';
 
 const isBothSame = function(string, position, length) {
   const left = string[position-length];
@@ -16,36 +16,58 @@ const isBothSame = function(string, position, length) {
   return (left === right);
 };
 
-const getLength = function(string, center) {
+const isNextSame = function(string, center1, center2, length) {
+  const left = string[center1-length];
+  const right = string[center2+length];
+  
+  if (left === undefined || right === undefined) return false;
+  return (left === right);
+};
+
+const getOddPalinedromeLength = function(string, center) {
   for (let i = 0; i < string.length; i++) {
     if (isBothSame(string, center, i)) continue;
     return i;
   }
+  return string.length;
 };
 
-const isEvenPalindrome = function(string) {
-  return !(string.length % 2);
-};
-
-const longestPalidrome = function(string) {
-  let position;
-  let longest = 0;
+const getEvenPalinedromeLength = function(string, center) {
   for (let i = 0; i < string.length; i++) {
-    const length = getLength(string, i); 
-    if (longest < length) {
-      longest = length;
-      position = i;
+    const isSameCharacter = isNextSame(string, center, center+1, i);
+    if (isSameCharacter) continue;
+    return i;
+  }
+};
+
+const longestPalindrome = function(string) {
+  let positionOdd;
+  let longestOdd = 0; 
+  let longestEven = 0; 
+  let positionEven;
+
+  for (let i = 0; i < string.length; i++) {
+    const oddPalinedrome = getOddPalinedromeLength(string, i); 
+    if (longestOdd < oddPalinedrome) {
+      longestOdd = oddPalinedrome;
+      positionOdd = i;
+    }
+
+    const evenPalinedrome = getEvenPalinedromeLength(string, i); 
+    if (longestEven < evenPalinedrome) {
+      longestEven = evenPalinedrome;
+      positionEven = i;
     }
   }
 
-  if (isEvenPalindrome(string)) {
-    return '짝수';
+  if (longestOdd > longestEven) {
+    return string.substring(positionOdd - longestOdd + 1, positionOdd + longestOdd);
   } else {
-    return string.substring(position - (longest - 1), position + longest);
+    return string.substring(positionEven - longestEven + 1, positionEven + 1 + longestEven);
   }
 };
 
-// console.log(longestPalidrome(input1));
-console.log(longestPalidrome(input2)); 
-// console.log(longestPalidrome(input3)); 
-// console.log(longestPalidrome(input4)); 
+console.log(longestPalindrome(input1));
+console.log(longestPalindrome(input2)); 
+console.log(longestPalindrome(input3)); 
+console.log(longestPalindrome(input4)); 
