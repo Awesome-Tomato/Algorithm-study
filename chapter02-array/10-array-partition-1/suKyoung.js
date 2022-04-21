2db6c8608e6a152aa111909434e5ee276484b90f
SuKyoung Shin <sukyoung.dev@gmail.com>
2022-04-22 00:17:12 +0900

    feat : 배열 9, 10번 문제 풀이 추가
    - 9번 : 속도개선 필요

---
+++ chapter02-array/10-array-partition-1/suKyoung.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+const nums1 = [1,4,3,2]; 
+const nums2 = [6,2,6,5,1,2]; 
+
+const arrayPairSum = function(nums) {
+  nums = nums.sort(sortArray);
+  let result = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (i % 2 === 0) {
+      result += nums[i];
+    };
+  }
+  return result;
+};
+
+const sortArray = function(a, b) {
+  if (a < b) return -1;
+  if (a > b) return 1;
+  return 0;
+};
+
+console.log(arrayPairSum(nums1));
+console.log(arrayPairSum(nums2));
