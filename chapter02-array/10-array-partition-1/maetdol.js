ff2d8bf1608bab3dfee6fd22e8e3015d56e166a1
MaetDol <rndka9@gmail.com>
2022-04-26 23:37:55 +0900

    fix, 잘못된 비교 수정
    i가 1부터 시작되게 변경되어 비교문도 i 가 nums.length보다 작은지 검사해야하 하는데,
    0이랑 비교해 무한루프를 돌았었습니다

--- chapter02-array/10-array-partition-1/maetdol.js
+++ chapter02-array/10-array-partition-1/maetdol.js
@@ -2,12 +2,12 @@
  * @param {number[]} nums
  * @return {number}
  */
-var arrayPairSum = function(nums) {
-    nums.sort((a, b) => b - a);
-    let sum = 0;
-    // nums 의 길이는 짝수, 짝수번째 숫자들의 합을 구한다
-    for(let i=1; i >= 0; i+=2) {
-        sum += nums[i];
-    }
-    return sum
+var arrayPairSum = function (nums) {
+  nums.sort((a, b) => b - a);
+  let sum = 0;
+  // nums 의 길이는 짝수, 짝수번째 숫자들의 합을 구한다
+  for (let i = 1; i < nums.length; i += 2) {
+    sum += nums[i];
+  }
+  return sum;
 };
