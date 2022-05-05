ec40f65cba06a3b29805238455ba0dd14e738d67
MaetDol <rndka9@gmail.com>
2022-05-06 01:42:54 +0900

    chore: 주석 추가

--- chapter03-linked-list/15-reverse-linked-list/maetdol.js
+++ chapter03-linked-list/15-reverse-linked-list/maetdol.js
@@ -13,6 +13,7 @@ var reverseList = function (head) {
   let prevNode = null;
   while (head) {
     const next = head.next;
+    // 현재 노드의 다음을 이전노드로 변경한다
     head.next = prevNode;
 
     prevNode = head;
