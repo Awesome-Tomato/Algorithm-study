c89f2fa43f4f03cada01e97f9c49ab49139b3573
FEhyoeun <khyoeun92@gmail.com>
2022-05-14 10:03:53 +0900

    13 - palindrome-linked-list

--- chapter03-linked-list/13-palindrome-linked-list/FEhyoeun.js
+++ chapter03-linked-list/13-palindrome-linked-list/FEhyoeun.js
@@ -11,35 +11,30 @@
  */
 var isPalindrome = function(head) {
     let length = 0
-    let front = []
-    let back = []
     let temp = []
 
-
-    while(head.next !== null) {
+    while(head) {
         temp.push(head.val)
         head = head.next
         length++
     }
-
-    // length += 1
-    temp.push(head.val)
+    length -= 1
 
     let end = 0
     if(length % 2 !== 0) end = Math.floor(length / 2)
     else end = Math.floor(length / 2) - 1
 
-
+    let front = []
     for(let i=0; i<=Math.floor(length / 2); i++) {
         front.push(temp[i])
     }
 
+    let back = []
     for(let j=length; j>end; j--) {
         back.push(temp[j])
     }
 
-    console.log(length, front, back)
-
+    console.log(front, back)
     return JSON.stringify(front) == JSON.stringify(back) ? true : false
 
 };
